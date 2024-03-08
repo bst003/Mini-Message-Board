f8e3ac5b6d4bf66e3dd1e56923c383297db23010
bst003 <bstanner33@yahoo.com>
2024-03-07 23:38:14 -0500

    Create new route and add nodemon

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET new message form. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.send("respond with a resource");
 });
 
 module.exports = router;
